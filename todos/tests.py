cae3c16a121c1b695193f6f75ea184ba98f89871
Sheersh . <itsme.sheersh@gmail.com>
2025-11-04 10:39:50 +0530

    hi

--- todos/tests.py
+++ todos/tests.py
@@ -5,6 +5,6 @@ from django.test import TestCase
 <<<<<<< HEAD
 class test_class123():
 =======
-class test_class1():
+class test_class12():
 >>>>>>> b0a7d35c99c50dd059d4542c9fdf94b2a3a580f3
     pass
